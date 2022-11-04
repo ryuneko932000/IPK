8be0d2bef1a8ce30fa16354460f026a269fe44af
ryuneko932000 <kokeisnocturno2000@gmail.com>
2022-11-04 15:20:25 +0100

    Removed "using namespace std" and removed c++11 initializations

--- Aufgaben /Exercise 1/mitternachtsformel.cc
+++ Aufgaben /Exercise 1/mitternachtsformel.cc
@@ -1,42 +1,41 @@
 #include <iostream>
 #include <cmath>
 #include <string>
-using namespace std;
 
 // Benutzen Sie "-std=c++11" als Flag im Terminal. 
 
 int main(){
-    double a{};
-    double b{};
-    double c{};
+    double a;
+    double b;
+    double c;
 
-    cout << "Write the first coefficient (x1): "; 
-    cin >> a; 
-    cout << "Write the second coefficient (x2): ";
-    cin >> b; 
-    cout << "Write the third coefficient (x3): ";
-    cin >> c; 
-    double root    {sqrt(pow(b, 2) - 4*a*c)};
-    double answer1 {(-b + root)/(2*a)};
-    double answer2 {(-b - root)/(2*a)};
+    std::cout << "Write the first coefficient (x1): "; 
+    std::cin >> a; 
+    std::cout << "Write the second coefficient (x2): ";
+    std::cin >> b; 
+    std::cout << "Write the third coefficient (x3): ";
+    std::cin >> c; 
+    double root    = sqrt(pow(b, 2) - 4*a*c);
+    double answer1 = (-b + root)/(2*a);
+    double answer2 = (-b - root)/(2*a);
 
     if ((a == 0 && b == 0)){
         
-        string KB; 
-        cout << "Your first two coefficients are zero, this will run into an error!" << endl; 
-        cout << "Enter any key to continue or press CTRL-C to quit" << endl;
-        cin >> KB;
+        std::string KB; 
+        std::cout << "Your first two coefficients are zero, this will run into an error!" << std::endl; 
+        std::cout << "Press CTRL-C to quit or enter any key to continue:" << std::endl;
+        std::cin >> KB;
 
     }
 
     if (sqrt(pow(b, 2) - 4*a*c < 0)){
-        string KB;
-        cout << "The chosen numbers will run an imaginary result..." << endl; 
-        cout << "Press CTRL-C to quit or enter any key to continue: " << endl; 
-        cin >> KB; 
+        std::string KB;
+        std::cout << "The chosen numbers will run an imaginary result..." << std::endl; 
+        std::cout << "Press CTRL-C to quit or enter any key to continue: " << std::endl; 
+        std::cin >> KB; 
     }
 
-    cout << "First Root: " << answer1 << endl; 
-    cout << "Second Root: " << answer2 << endl; 
+    std::cout << "First Root: " << answer1 << std::endl; 
+    std::cout << "Second Root: " << answer2 << std::endl; 
 
 }
